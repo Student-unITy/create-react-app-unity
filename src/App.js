88ca45cbf68e1e8ddbe045af005ed5d7208da702
Dominik Joppa <domjoppa@gmail.com>
2022-01-22 09:33:03 +0100

    React 17 - update Tailwindcss 3 :+1:

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import Home from './components/page';
-import s from './components/page/s';
-
-import Navbar from './components/menu/Navbar';
-import Dropdown from './components/menu/Dropdown';
-
-import Footer from './components/global/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  useEffect(() => {
-    const hideMenu = () => {
-      if (window.innerWidth > 768 && isOpen) {
-        setIsOpen(false);
-        console.log('i resized');
-      }
-    };
-
-    window.addEventListener('resize', hideMenu);
-
-    return () => {
-      window.removeEventListener('resize', hideMenu);
-    };
-  });
-
   return (
-    <>
-      <div className="container mx-auto bg-white">
-        <Navbar toggle={toggle} />
-      </div>
-      <Dropdown isOpen={isOpen} toggle={toggle} />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/s' exact component={s} />
-      </Switch>
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
